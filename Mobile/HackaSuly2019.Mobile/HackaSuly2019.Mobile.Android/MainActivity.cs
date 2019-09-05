869aab3c93b88a2b96fad693cd0532c3f319a3a6
Muhammad Azeez <muhammad-azeez@outlook.com>
2019-09-06 01:49:11 +0300

    we can now get images (camera and gallery) only android is configured right now

--- Mobile/HackaSuly2019.Mobile/HackaSuly2019.Mobile.Android/MainActivity.cs
+++ Mobile/HackaSuly2019.Mobile/HackaSuly2019.Mobile.Android/MainActivity.cs
@@ -19,15 +19,18 @@ namespace HackaSuly2019.Mobile.Droid
 
             base.OnCreate(savedInstanceState);
 
+            Plugin.CurrentActivity.CrossCurrentActivity.Current.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.SetFlags("Shell_Experimental", "Visual_Experimental", "CollectionView_Experimental", "FastRenderers_Experimental");
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
+            
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
---
+++ Mobile/HackaSuly2019.Mobile/HackaSuly2019.Mobile/Views/WelcomePage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HackaSuly2019.Mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WelcomePage : ContentPage
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void SetPhoto(MediaFile file)
+        {
+            if (file == null)
+            {
+                return;
+            }
+
+            selectedImage.Source = ImageSource.FromStream(() => file.GetStream());
+        }
+
+        private async void CameraButton_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (CrossMedia.Current.IsCameraAvailable == false)
+            {
+                await DisplayAlert("Not supported", "Your device does not support this functionality", "Ok");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+            {
+                DefaultCamera = CameraDevice.Front
+            });
+
+            SetPhoto(file);
+        }
+
+        private async void GalleryButton_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (CrossMedia.Current.IsPickPhotoSupported == false)
+            {
+                await DisplayAlert("Not supported", "Your device does not support this functionality", "Ok");
+                return;
+            }
+
+            var file = await CrossMedia.Current.PickPhotoAsync(new PickMediaOptions
+            {
+                PhotoSize = PhotoSize.Medium,
+            });
+
+            SetPhoto(file);
+        }
+    }
+}
