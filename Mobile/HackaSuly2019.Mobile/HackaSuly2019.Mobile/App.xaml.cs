183571c4302e4923ef1449753cc20823b98a5c4d
Muhammad Azeez <muhammad-azeez@outlook.com>
2019-09-06 00:39:27 +0300

    create mobile app project

---
+++ Mobile/HackaSuly2019.Mobile/HackaSuly2019.Mobile/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using HackaSuly2019.Mobile.Services;
+using HackaSuly2019.Mobile.Views;
+
+namespace HackaSuly2019.Mobile
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
