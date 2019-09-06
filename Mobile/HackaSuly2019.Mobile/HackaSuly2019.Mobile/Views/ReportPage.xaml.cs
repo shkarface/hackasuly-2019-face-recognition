3cc625979a5549f7bad190427ae9bfa804ce8c1b
Muhammad Azeez <muhammad-azeez@outlook.com>
2019-09-06 10:26:31 +0300

    show scan animation when submitting report

--- Mobile/HackaSuly2019.Mobile/HackaSuly2019.Mobile/Views/ReportPage.xaml.cs
+++ Mobile/HackaSuly2019.Mobile/HackaSuly2019.Mobile/Views/ReportPage.xaml.cs
@@ -4,6 +4,7 @@ using Plugin.Media.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,8 @@ namespace HackaSuly2019.Mobile.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ReportPage : ContentPage
     {
+        private Stream _image;
+
         private ReportPageState _state;
         public string State
         {
@@ -102,13 +105,7 @@ namespace HackaSuly2019.Mobile.Views
                 watch.Start();
 
                 profilePicture.Source = ImageSource.FromStream(() => file.GetStream());
-                var stream = file.GetStream();
-                var uri = await ImageUploadHelper.UploadFileAsync(stream);
-                watch.Stop();
-
-                await Xamarin.Essentials.Browser.OpenAsync(uri.AbsoluteUri);
-
-                await DisplayAlert($"File uploaded! ({watch.ElapsedMilliseconds:n0} ms)", uri.ToString(), "OK");
+                _image = file.GetStream();
             }
             catch (Exception ex)
             {
@@ -122,20 +119,57 @@ namespace HackaSuly2019.Mobile.Views
 
         private void SetBusy()
         {
-            //cameraButton.IsEnabled = false;
-            //galleryButton.IsEnabled = false;
-            //progressGrid.IsVisible = true;
+            selectImageButton.IsEnabled = false;
+            reportButton.IsEnabled = false;
+            progressGrid.IsVisible = true;
         }
         private void SetNotBusy()
         {
-            //cameraButton.IsEnabled = true;
-            //galleryButton.IsEnabled = true;
-            //progressGrid.IsVisible = false;
+            selectImageButton.IsEnabled = true;
+            reportButton.IsEnabled = true;
+            progressGrid.IsVisible = false;
         }
 
-        private void ReportButton_Clicked(object sender, EventArgs e)
+        private async void ReportButton_Clicked(object sender, EventArgs e)
         {
+            if (_image is null)
+            {
+                await DisplayAlert("Please select an image", "Please select an image from camera or gallery.", "Ok");
+                return;
+            }
 
+            var missingFields = new List<string>();
+            if (string.IsNullOrEmpty(nameEntry.Text))
+                missingFields.Add("Name");
+
+            if (string.IsNullOrEmpty(phoneEntry.Text))
+                missingFields.Add("Phone number");
+
+            if (maleRadioButton.IsChecked == false && femaleRadioButton.IsChecked == false)
+                missingFields.Add("Missing person's sex");
+
+            if (missingFields.Any())
+            {
+                await DisplayAlert("Required Fields", $"Please fill in the required fields: {string.Join(", ", missingFields)}", "Ok");
+                return;
+            }
+
+            try
+            {
+                SetBusy();
+
+                var uri = await ImageUploadHelper.UploadFileAsync(_image);
+
+                await Xamarin.Essentials.Browser.OpenAsync(uri.AbsoluteUri);
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+            }
+            finally
+            {
+                SetNotBusy();
+            }
         }
     }
 }
